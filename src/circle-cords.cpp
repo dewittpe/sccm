6bd5aac845a25681934eb7703ce04289de121c3b
Peter DeWitt (inspiron-5323) <dewittpe@gmail.com>
2016-09-07 22:55:26 -0600

    polar2cartesian and cartesian2polar all done in .cpp files.

--- src/circle-cords.cpp
+++ src/circle-cords.cpp
@@ -1,12 +1,31 @@
+// [[Rcpp::depends(RcppArmadillo)]]
+#include <RcppArmadillo.h>
 #include <Rcpp.h>
 #include "sccm.h"
 
+
+//' Translate between Polar and Cartesian Coordinates.
+//'
+//' Given (x,y) return (r, theta), or visa-versa.
+//'
+//' @param r a numeric vector of radii
+//' @param theta a numeric vector of angles (in radians)
+//' @export
+//' @rdname polar2cartesian
 // [[Rcpp::export]]
-Rcpp::NumericMatrix polar2cartesian_cpp(Rcpp::NumericVector r, Rcpp::NumericVector theta)
+Rcpp::NumericMatrix polar2cartesian(arma::vec r, arma::vec theta)
 {
 
   if (r.size() != theta.size()) { 
-    throw std::invalid_argument("length(r) != length(theta)");
+    if (r.size() == 1) { 
+      r.resize(theta.size());
+      r.fill(r(0));
+    } else if (theta.size() == 1) {
+      theta.resize(r.size());
+      theta.fill(theta(0));
+    } else { 
+      throw std::invalid_argument("length(r) == length(theta), or length(r) == 1, or length(theta) == 1 required.");
+    }
   }
 
   Rcpp::NumericMatrix out(r.size(), 2);
@@ -19,12 +38,27 @@ Rcpp::NumericMatrix polar2cartesian_cpp(Rcpp::NumericVector r, Rcpp::NumericVect
   return out;
 }
 
+//' @param x a numeric vector of x coordinates
+//' @param y a numeric vector of y coordinates
+//' 
+//' @return A numeric matrix
+//' 
+//' @export
+//' @rdname polar2cartesian
 // [[Rcpp::export]]
-Rcpp::NumericMatrix cartesian2polar_cpp(Rcpp::NumericVector x, Rcpp::NumericVector y)
+Rcpp::NumericMatrix cartesian2polar(arma::vec x, arma::vec y)
 {
 
   if (x.size() != y.size()) { 
-    throw std::invalid_argument("length(x) != length(y)");
+    if (x.size() == 1) { 
+      x.resize(y.size());
+      x.fill(x(0));
+    } else if (y.size() == 1) {
+      y.resize(x.size());
+      y.fill(y(0));
+    } else { 
+      throw std::invalid_argument("length(r) == length(y), or length(r) == 1, or length(y) == 1 required.");
+    }
   }
 
   Rcpp::NumericMatrix out(x.size(), 2);

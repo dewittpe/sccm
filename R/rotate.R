9fb710d0342114714a7615dd803ce07ca26eb7e8
Peter DeWitt (latitude) <peter.dewitt@ucdenver.edu>
2017-02-22 09:12:33 +0000

    removed the unused "counter" argument from the rotate function

--- R/rotate.R
+++ R/rotate.R
@@ -1,18 +1,28 @@
 #' Rotate
 #'
-#' Rotate a polygon or convex hull
+#' Rotate a polygon or convex hull.
+#'
+#' The default call to \code{rotate} simply moves the first vertex to the last
+#' row of the vertex matrix and moves all the other rows up one, i.e.,
+#'
+#' \code{x$vertices <- x$vertices[c(2:nrow(x$vertices), 1), ]}.
+#'
+#' For \code{times > 1L}, the above code line is evaluated \code{times} times.
+#'
+#' The \code{vertices} and \code{beta} elements of \code{sccm_pg} objects are
+#' updated.  \code{sccm_ch} objects have \code{vertices}, \code{beta}, and
+#' \code{index} updated.
 #'
 #' @param x a \code{sccm_ch} or \code{sccm_pg} object
-#' @param counter (logical, default = \code{TRUE}), rotate counter clockwise
 #' @param times (integer, default = 1), number vertices to rotate through
 #'
 #' @export
-rotate <- function(x, counter = TRUE, times = 1L) {
+rotate <- function(x, times = 1L) {
   UseMethod("rotate")
 }
 
 #' @export
-rotate.sccm_pg <- function(x, counter = TRUE, times = 1L) {
+rotate.sccm_pg <- function(x, times = 1L) {
   for( i in 1:times) {
     idx <- c(seq(2, nrow(x$vertices)), 1)
     x$vertices <- x$vertices[idx, ]
@@ -22,7 +32,7 @@ rotate.sccm_pg <- function(x, counter = TRUE, times = 1L) {
 }
 
 #' @export
-rotate.sccm_ch <- function(x, counter = TRUE, times = 1L) { 
+rotate.sccm_ch <- function(x, times = 1L) {
   for(i in 1:times) {
     idx <- c(seq(2, nrow(x$vertices)), 1)
     x$vertices <- x$vertices[idx, ]
